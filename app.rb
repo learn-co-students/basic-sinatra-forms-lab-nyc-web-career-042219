b11e9344821dee0ecf7a3b29123f3223d46eb38f
henryk1229 <henryk1229@gmail.com>
2019-05-15 11:22:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require 'sinatra/base'
 
+
 class App < Sinatra::Base
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = params
+    @coach = @team[:coach]
+    @name = @team[:name]
+    @pg = @team[:pg]
+    @sg = @team[:sg]
+    @sf = @team[:sf]
+    @pf = @team[:pf]
+    @c = @team[:c]
+
+    erb :team
+  end
+
+  # get '/team' do
+  #   erb :team
+  # end
+
+
+
+
+
+
 
 
 end
